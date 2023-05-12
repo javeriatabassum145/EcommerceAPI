e2d4d81e285715ba586ca5853297efcc803b5f0a
Javeria Tabassum <javeria.tabassum145@gmail.com>
2023-05-13 01:54:17 +0500

    Models added

---
+++ Models/Brands.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceCRUDAPI.Models
+{
+    public class Brands
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? Category { get; set; }
+
+        [MaxLength(1,ErrorMessage = "Length should be 1"),DisplayName("Active")]
+        public int IsActive { get; set; }
+
+    }
+}
