76128065a238fd34e7d85b5a289381d73dc93092
javeria.tabassum <javeria.tabassum@kaispe.com>
2023-05-15 18:48:45 +0500

    httpget added

--- Controllers/BrandController.cs
+++ Controllers/BrandController.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EcommerceCRUDAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EcommerceCRUDAPI.Controllers
 {
-    public class BrandController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly BrandContext _dbContext;
+
+        public BrandController(BrandContext dbContext){
+            _dbContext= dbContext;
+}
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Brand>>> GetBrands()
         {
-            return View();
+            if (_dbContext.Brands == null)
+            {
+                return NotFound();
+            }
+            return await _dbContext.Brands.ToListAsync();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Brand>>> GetBrands(int id)
+        {
+            if (_dbContext.Brands == null)
+            {
+                return NotFound();
+            }
+            return await _dbContext.Brands.FindAsync(id);
+
+            if (brand == null)
+            {
+                return NotFound();
+            }
+            return brand;
         }
     }
 }
